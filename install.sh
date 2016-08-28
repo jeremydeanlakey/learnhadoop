a3ea68a6dc141dfc282e89c87558895784412aef
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-08-28 07:02:25 -0600

    environment setup script

---
+++ install.sh
@@ -0,0 +1,12 @@
+# sudo this script so it can modify /usr/local
+
+cd /usr/local
+
+# see http://www.apache.org/dyn/closer.cgi/hadoop/common/
+wget http://mirror.cogentco.com/pub/apache/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz
+
+tar tar -xzf hadoop-2.7.3.tar.gz
+
+mv hadoop-2.7.3 hadoop
+
+
