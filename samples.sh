c65a9610b3688b6691913b9b3d94f46f47f46d98
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-08-31 06:58:31 -0600

    fix sample script

--- samples.sh
+++ samples.sh
@@ -2,6 +2,7 @@ mkdir input
 mkdir output
 cp $HADOOP_HOME/*.txt input 
 
+export PATH=$PATH:/usr/local/hadoop/bin
 
 hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.3.jar wordcount input output
 
