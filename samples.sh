6880552c9c7804879c5aba914e036f85e604fc20
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-09-02 06:54:00 -0600

    fix JAVA_HOME

--- samples.sh
+++ samples.sh
@@ -4,5 +4,7 @@ cp $HADOOP_HOME/*.txt input
 
 export PATH=$PATH:/usr/local/hadoop/bin
 
+export JAVA_HOME=/opt/java/32/jdk1.8.0_31   # this actually needs to go in the hadoop-env.sh
+
 hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.3.jar wordcount input output
 
