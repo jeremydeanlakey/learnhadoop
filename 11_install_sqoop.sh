d28ce0f5b0325e31e8d902253481f483aabc6c0e
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-09-22 20:58:10 -0600

    sqoop environment

--- 11_install_sqoop.sh
+++ 11_install_sqoop.sh
@@ -11,3 +11,12 @@ export PATH=\$PATH:\$SQOOP_HOME/bin
 
 source .bashrc
 
+cp $SQOOP_HOME/conf/sqoop-env-template.sh $SQOOP_HOME/conf/sqoop-env.sh
+
+
+echo "
+export HADOOP_COMMON_HOME=/usr/local/hadoop 
+export HADOOP_MAPRED_HOME=/usr/local/hadoop
+" >> $SQOOP_HOME/conf/sqoop-env.sh
+
+
